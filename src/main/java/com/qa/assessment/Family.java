f1865ef3d787333da72d113d864feb5e7944102e
lly <lly@lly-PC>
2019-03-31 23:43:00 +0100

    added gender deduction to male and female methods and made it work even where one man had children with multiple women or the reverse
    additionally added a recursive check that makes sure that a person
    cannot be his own ancestor
    
    tested both of these additions

--- src/main/java/com/qa/assessment/Family.java
+++ src/main/java/com/qa/assessment/Family.java
@@ -3,19 +3,29 @@ package com.qa.assessment;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Family {
 
 	private List<String> males = new ArrayList<>();
 	private List<String> females = new ArrayList<>();
-	private HashMap<String, List<String>> parents = new HashMap<String, List<String>>();
-	private HashMap<String, List<String>> children = new HashMap<String, List<String>>();
+	private HashMap<String, List<String>> parentList = new HashMap<String, List<String>>();
+	private HashMap<String, List<String>> childrenList = new HashMap<String, List<String>>();
 
 	public boolean male(String name) {
 		if (males.contains(name) || females.contains(name)) {
 			return false;
-		} else {
+		}
+		else {
 			males.add(name);
+			if(this.getChildrenOf(name).size() != 0) {
+				for(String child : this.getChildrenOf(name)) {
+					List<String> parents = this.getParentsOf(child);
+					if (parents.size() != 1) {
+						Boolean temp = (parents.get(0) == name) ? females.add(parents.get(1)) : females.add(parents.get(0));
+					}
+				}
+			}
 			return true;
 		}
 	}
@@ -23,53 +33,103 @@ public class Family {
 	public boolean female(String name) {
 		if (females.contains(name) || males.contains(name)) {
 			return false;
-		} else {
+		}
+		else {
 			females.add(name);
+			if(this.getChildrenOf(name).size() != 0) {
+				for(String child : this.getChildrenOf(name)) {
+					List<String> parents = this.getParentsOf(child);
+					if (parents.size() != 1) {
+						Boolean temp = (parents.get(0) == name) ? males.add(parents.get(1)) : males.add(parents.get(0));
+					}
+				}
+			}
 			return true;
 		}
 	}
 
 	public boolean isMale(String name) {
-		if (males.contains(name)) {
-			return true;
-		} else {
-			return false;
-		}
+		return males.contains(name) ? true : false;
 	}
 
 	public boolean isFemale(String name) {
-		if (females.contains(name)) {
-			return true;
-		} else {
-			return false;
-		}
+		return females.contains(name) ? true : false;
 	}
 
-	public boolean setParent(String childName, String parentName) {
+	public boolean setParentOf(String childName, String parentName) {
+
+		List<String> parents;
+		List<String> children;
+		
 		if (childName == parentName) {
 			return false;
-		} else if (parents.get(childName) == null) {
-			List<String> parent = new ArrayList<>();
-			parent.add(parentName);
-			parents.put(childName, parent);
-			return true;
-		} else if (parents.get(childName).size() == 1 && parents.get(childName).get(0) != parentName) {
-			List<String> parent = parents.get(childName);
-			parent.add(parentName);
-			parents.replace(childName, parent);
-			return true;
+		}
+		else if (this.parentOfParent(this.getChildrenOf(childName), parentName)) {
+			return false;
+		}
+		else if (parentList.get(childName) == null) {
+			parents = new ArrayList<>();
+			parents.add(parentName);
+			parentList.put(childName, parents);
+		} 
+		else if (parentList.get(childName).size() == 1 && parentList.get(childName).get(0) != parentName) {
+			parents = parentList.get(childName);
+			parents.add(parentName);
+			parentList.replace(childName, parents);
+
+			// The block below attempts to infer genders of parents
+
+			if (males.contains(parentName)) {
+				females.add(parentList.get(childName).get(0));
+			} 
+			else if (females.contains(parentName)) {
+				males.add(parentList.get(childName).get(0));
+			} 
+			else if (males.contains(parentList.get(childName).get(0))) {
+				females.add(parentName);
+			} 
+			else if (females.contains(parentList.get(childName).get(0))) {
+				males.add(parentName);
+			}
 		} else {
 			return false;
 		}
+		
+		// If the conditions did not stop the process this will 
+		// proceed to update the children list
+		
+		if (childrenList.get(parentName) == null) {
+			children = new ArrayList<>();
+			children.add(childName);
+			childrenList.put(parentName, children);
+			return true;
+		} 
+		else {
+			children = childrenList.get(parentName);
+			children.add(childName);
+			childrenList.replace(parentName, children);
+			return true;
+		}
 	}
 
-	public String[] getParents(String name) {
-
-		return null;
+	public List<String> getParentsOf(String name) {
+		return parentList.containsKey(name) ? parentList.get(name) : new ArrayList<String>();
 	}
 
-	public String[] getChildren(String name) {
-
-		return null;
+	public List<String> getChildrenOf(String name) {
+		return childrenList.containsKey(name) ? childrenList.get(name) : new ArrayList<String>();
+	}
+	
+	//The method below recursively collects everyone related to a list of children and once it stops, checks if the new list contains the name
+	
+	private boolean parentOfParent(List<String> children, String name) {
+		List<String> offspring = new ArrayList<>();
+		offspring.addAll(children);
+		for(String child : children) {
+			if (this.getChildrenOf(child).size() != 0 && !offspring.contains(name)) {
+				offspring.addAll(this.getChildrenOf(child));
+			}
+		}
+		return offspring.equals(children) ? offspring.contains(name) : this.parentOfParent(offspring, name);
 	}
 }
--- src/test/java/com/qa/tests/FamilyTest.java
+++ src/test/java/com/qa/tests/FamilyTest.java
@@ -54,64 +54,122 @@ public class FamilyTest {
 	
 	@Test
 	public void setParentPassTest() {
-		fam.setParent("Sam", "John");
-		assertTrue(fam.setParent("Sam", "Mary"));
+		fam.setParentOf("Sam", "John");
+		assertTrue(fam.setParentOf("Sam", "Mary"));
 	}
 	
 	@Test
 	public void setSameParentTest() {
-		fam.setParent("Sam", "John");
-		assertFalse(fam.setParent("Sam", "John"));
+		fam.setParentOf("Sam", "John");
+		assertFalse(fam.setParentOf("Sam", "John"));
 	}
 	
 	@Test
 	public void setTooManyParentsTest() {
-		fam.setParent("Sam", "John");
-		fam.setParent("Sam", "Mary");
-		assertFalse(fam.setParent("Sam", "Anna"));
+		fam.setParentOf("Sam", "John");
+		fam.setParentOf("Sam", "Mary");
+		assertFalse(fam.setParentOf("Sam", "Anna"));
 	}
 	
 	@Test
 	public void setParentOfSelfTest() {
-		assertFalse(fam.setParent("Sam", "Sam"));
+		assertFalse(fam.setParentOf("Sam", "Sam"));
 	}
 	
 	@Test
 	public void newNameGetChildrenTest() {
-		assertEquals(0, fam.getChildren("Sam").length);
+		assertEquals(0, fam.getChildrenOf("Sam").size());
 	}
 	
 	@Test
 	public void newNameGetParentsTest() {
-		assertEquals(0, fam.getParents("Sam").length);
+		assertEquals(0, fam.getParentsOf("Sam").size());
 	}
 	
 	@Test
 	public void getParentsTest() {
-		fam.setParent("Sam", "John");
-		fam.setParent("Sam", "Mary");
-		assertEquals("[John, Mary]", fam.getParents("Sam").toString());
+		fam.setParentOf("Sam", "John");
+		fam.setParentOf("Sam", "Mary");
+		assertEquals("[John, Mary]", fam.getParentsOf("Sam").toString());
 	}
 	
 	@Test
 	public void getChildrenTest() {
-		fam.setParent("Sam", "John");
-		fam.setParent("Sam", "Mary");
-		fam.setParent("Anna", "John");
-		fam.setParent("Anna", "Mary");
-		assertEquals("[Sam, Anna]", fam.getChildren("John").toString());
-		assertEquals("[Sam, Anna]", fam.getChildren("Mary").toString());
+		fam.setParentOf("Sam", "John");
+		fam.setParentOf("Sam", "Mary");
+		fam.setParentOf("Anna", "John");
+		fam.setParentOf("Anna", "Mary");
+		assertEquals("[Sam, Anna]", fam.getChildrenOf("John").toString());
+		assertEquals("[Sam, Anna]", fam.getChildrenOf("Mary").toString());
 	}
 	
+	@Test
+	public void genderDeductionTest() {
+		fam.male("John");
+		fam.setParentOf("Sam", "John");
+		fam.setParentOf("Sam", "Mary");
+		assertTrue(fam.isFemale("Mary"));
+		assertFalse(fam.isMale("Mary"));
+	}
 	
+	@Test
+	public void childrenFromDifferentMothersTest() {
+		fam.setParentOf("Sam", "John");
+		fam.setParentOf("Sam", "Mary");
+		fam.setParentOf("Tom", "John");
+		fam.setParentOf("Tom", "Anna");
+		assertEquals("[John, Mary]", fam.getParentsOf("Sam").toString());
+		assertEquals("[John, Anna]", fam.getParentsOf("Tom").toString());
+	}
 	
+	@Test
+	public void parentIsAlsoGrandparentTest() {
+		fam.setParentOf("Sam", "John");
+		fam.setParentOf("Sam", "Mary");
+		fam.setParentOf("Mary", "John");
+		fam.setParentOf("Mary", "Anna");
+		assertEquals("[John, Mary]", fam.getParentsOf("Sam").toString());
+		assertEquals("[John, Anna]", fam.getParentsOf("Mary").toString());
+	}
 	
+	@Test
+	public void caseSensetiveTest() {
+		fam.setParentOf("Sam", "John");
+		assertTrue(fam.setParentOf("Sam", "john"));
+	}
 	
+	@Test
+	public void setBothParentsGenderTest() {
+		fam.setParentOf("Vera", "George");
+		fam.setParentOf("Vera", "Vanessa");
+		fam.setParentOf("Mary", "John");
+		fam.setParentOf("Mary", "Vanessa");
+		fam.female("Vanessa");
+		assertFalse(fam.female("George"));
+		assertTrue(fam.isMale("George"));
+		assertFalse(fam.female("John"));
+		assertTrue(fam.isMale("John"));
+	}
 	
-	
-	
-	
-	
+	@Test
+	public void assessmentPaperTest() {
+		assertTrue(fam.setParentOf("Frank", "Morgan"));
+		assertTrue(fam.setParentOf("Frank", "Dylan"));
+		assertTrue(fam.male("Dylan"));
+		assertTrue(fam.setParentOf("Joy", "Frank"));
+		assertTrue(fam.male("Frank"));
+		assertFalse(fam.male("Morgan"));
+		assertTrue(fam.setParentOf("July", "Morgan"));
+		assertFalse(fam.isMale("Joy") || fam.isFemale("Joy"));
+		assertEquals("[Frank, July]", fam.getChildrenOf("Morgan").toString());
+		assertTrue(fam.setParentOf("Jennifer", "Morgan"));
+		assertEquals("[Frank, July, Jennifer]", fam.getChildrenOf("Morgan").toString());
+		assertEquals("[Frank]", fam.getChildrenOf("Dylan").toString());
+		assertEquals("[Morgan, Dylan]", fam.getParentsOf("Frank").toString());
+		assertFalse(fam.setParentOf("Morgan", "Frank"));
+		
+		assertFalse(fam.setParentOf("Morgan", "Joy")); //This was not in the example originally, but I added this to check recursion
+	}
 	
 	
 }
