65af9159f07e0305fccb3fb25ca68c1a605a8aef
lly <lly@lly-PC>
2019-03-31 21:10:17 +0100

    implemented some of the methods and conditions

--- src/main/java/com/qa/assessment/Family.java
+++ src/main/java/com/qa/assessment/Family.java
@@ -1,37 +1,73 @@
 package com.qa.assessment;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Family {
 
-	public boolean male(String name) {
+	private List<String> males = new ArrayList<>();
+	private List<String> females = new ArrayList<>();
+	private HashMap<String, List<String>> parents = new HashMap<String, List<String>>();
+	private HashMap<String, List<String>> children = new HashMap<String, List<String>>();
 
-		return true;
+	public boolean male(String name) {
+		if (males.contains(name) || females.contains(name)) {
+			return false;
+		} else {
+			males.add(name);
+			return true;
+		}
 	}
 
 	public boolean female(String name) {
-
-		return true;
+		if (females.contains(name) || males.contains(name)) {
+			return false;
+		} else {
+			females.add(name);
+			return true;
+		}
 	}
 
 	public boolean isMale(String name) {
-
-		return true;
+		if (males.contains(name)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public boolean isFemale(String name) {
+		if (females.contains(name)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-		return true;
-	}
-	
 	public boolean setParent(String childName, String parentName) {
-		
-		return true;
+		if (childName == parentName) {
+			return false;
+		} else if (parents.get(childName) == null) {
+			List<String> parent = new ArrayList<>();
+			parent.add(parentName);
+			parents.put(childName, parent);
+			return true;
+		} else if (parents.get(childName).size() == 1 && parents.get(childName).get(0) != parentName) {
+			List<String> parent = parents.get(childName);
+			parent.add(parentName);
+			parents.replace(childName, parent);
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
 	public String[] getParents(String name) {
 
 		return null;
 	}
-	
+
 	public String[] getChildren(String name) {
 
 		return null;
