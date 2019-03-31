c4668f9248ca1ce94f936a886beeeab789234ae5
lly <lly@lly-PC>
2019-03-31 20:02:01 +0100

    Set up some tests and the family class with empty methods

---
+++ src/main/java/com/qa/assessment/Family.java
@@ -0,0 +1,39 @@
+package com.qa.assessment;
+
+public class Family {
+
+	public boolean male(String name) {
+
+		return true;
+	}
+
+	public boolean female(String name) {
+
+		return true;
+	}
+
+	public boolean isMale(String name) {
+
+		return true;
+	}
+
+	public boolean isFemale(String name) {
+
+		return true;
+	}
+	
+	public boolean setParent(String childName, String parentName) {
+		
+		return true;
+	}
+	
+	public String[] getParents(String name) {
+
+		return null;
+	}
+	
+	public String[] getChildren(String name) {
+
+		return null;
+	}
+}
